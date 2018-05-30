2e9cf8a08d25233361a09a0b80eceb19b1135a22
Arkar <info@rangoontech.com>
2018-05-31 01:17:05 +0630

    web
    web

--- js/main.js
+++ js/main.js
@@ -1,397 +1,163 @@
-/*-----------------------------------------------------------------------------------
-/*
-/* Main JS
-/*
------------------------------------------------------------------------------------*/  
+$(document).ready(function(){
+  'use strict';
+  //===== Counter Up =====//
+  if ($.isFunction($.fn.counterUp)) {
+    $('.counter').counterUp({
+      delay: 10,
+      time: 2000
+    });
+  }
 
-(function($) {
+  //===== Accordion =====//
+  $('#acordn .acrdn-cnt').hide();
+  $('#acordn h4:first').addClass('active').next().slideDown(500).parent().addClass("activate");
+  $('#acordn h4').on("click", function(){
+    if ($(this).next().is(':hidden')) {
+      $('#acordn h4').removeClass('active').next().slideUp(500).parent().removeClass("activate");
+      $(this).toggleClass('active').next().slideDown(500).parent().toggleClass("activate");
+    }
+  });
 
-   /*---------------------------------------------------- */
-	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
-
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
-
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-
-      });     
-
-  	})
-
-   /*---------------------------------------------------- */
-	/* Final Countdown Settings
-	------------------------------------------------------ */
-	var finalDate = '2017/01/01';
-
-	$('div#counter').countdown(finalDate)
-   	.on('update.countdown', function(event) {
-
-   		$(this).html(event.strftime('<span>%D <em>days</em></span>' + 
-   										 	 '<span>%H <em>hours</em></span>' + 
-   										 	 '<span>%M <em>minutes</em></span>' +
-   										 	 '<span>%S <em>seconds</em></span>'));
-
-   });
-
-   /*----------------------------------------------------*/
-	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */  	 
-	$('input').placeholder() 
-	
-
-   /*----------------------------------------------------- */
-   /* Modals
-   ------------------------------------------------------- */   
-   $('.modal-toggles ul').on('click', 'a', function(e) {
-
-   	var html = $('html'),
-   		 main = $('main, footer'),
-   		 footer = $('footer'),           
-          curMod = $(this).attr('href'),  
-          modal = $(curMod),
-          modClose = modal.find('#modal-close');          
-         
-		main.fadeOut(500, function(){
-			$('html,body').scrollTop(0);
-        	modal.addClass('is-visible');
-      });  
-      
-      e.preventDefault();
-
-      // for old ie
-      if (html.hasClass('oldie')) {
-
-      	$(document).on('click', "#modal-close", function(evt) {
-	      	$('html,body').scrollTop(0); 
-	      	modal.removeClass('is-visible');
-	      	setTimeout(function() {      
-	        		main.fadeIn(500); 
-	        	}, 500);       
-	        	        
-	        	evt.preventDefault();
-      	});
-
+  //===== Sticky Header =====//
+  var menu_height = $('header').innerHeight();
+  $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll >= menu_height) {
+        $('.stck').addClass('stcky');
+      } else {
+        $('.stck').removeClass('stcky');
       }
-      // other browsers
-      else {
+  });
+  if ($('header').hasClass('stck')) {
+    $('main').css({'padding-top': menu_height});
+  }
 
-      	modClose.on('click', function(evt) {
-	      	$('html,body').scrollTop(0); 
-	      	modal.removeClass('is-visible');
-	      	setTimeout(function() {      
-	        		main.fadeIn(500); 
-	        	}, 500);       
-	        	        
-	        	evt.preventDefault();
-	      });
 
-      }     	
+  //===== Responsive Header =====//
+  $('.mnu-btn').on('click', function () {
+    $('.rspnsv-mnu').addClass('sldin');
+    return false;
+  });
+  $('.cls-btn').on('click', function () {
+    $('.rspnsv-mnu').removeClass('sldin');
+    return false;
+  });
+  $('.rspnsv-mnu li.menu-item-has-children > a').on('click', function () {
+    $(this).parent().siblings().children('ul').slideUp();
+    $(this).parent().siblings().removeClass('active');
+    $(this).parent().children('ul').slideToggle();
+    $(this).parent().toggleClass('active');
+    return false;
+  });
 
-   });
+  //===== Scroll Up Bar =====//
+  if ($.isFunction($.fn.scrollupbar)) {
+    $('header').scrollupbar();
+  }
 
-   /*---------------------------------------------------- */
-	/* Owl Carousel
-	------------------------------------------------------ */ 
-	$("#owl-slider").owlCarousel({
-        navigation: false,
-        pagination: true,
-        items: 4,
-        navigationText: false
+  //===== Scroll Function =====//
+  $(function() {
+    $('nav.one-page-func > ul > li > a,.rspnsv-mnu > ul > li > a,.clk-scrl').on('click',function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+        }
+      }
+      return false;
+    });
+  });
+
+  //===== LightBox =====//
+  if ($.isFunction($.fn.poptrox)) {
+    var foo = $('.lightbox');
+    foo.poptrox({usePopupCaption: true, usePopupNav: true});
+  }
+
+  //===== Select 2 =====//
+  if ($.isFunction($.fn.select2)) {
+    $('select').select2({});
+  }
+
+  //===== Custom Scrollbar =====//
+  if ($.isFunction($.fn.mCustomScrollbar)) {
+    $('.custom-scrollbar').mCustomScrollbar({theme:"light-thick"});
+  }
+
+  //===== Owl Carousel =====//
+  if ($.isFunction($.fn.owlCarousel)) {
+    //=== Apps Screenshots Carousel ===//
+    $('.app-scrn-caro').owlCarousel({
+      autoplay: true,
+      smartSpeed: 3000,
+      loop: true,
+      items: 3,
+      dots: false,
+      slideSpeed: 2000,
+      nav: true,
+      navText:['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'],
+      margin: 50,
+      center: true,
+      responsive: {
+        0: {items: 1,nav: false},
+        480: {items: 2,nav: false},
+        768: {items: 2,nav: false},
+        1200: {items: 3}
+      }
     });
 
+    //=== Testimonials Carousel ===//
+    $('.testi-caro-innr').owlCarousel({
+      autoplay: true,
+      smartSpeed: 3000,
+      loop: true,
+      items: 2,
+      dots: false,
+      slideSpeed: 2000,
+      nav: false,
+      margin: 70,
+      responsive: {
+        0: {items: 1},
+        480: {items: 1},
+        768: {items: 1},
+        1200: {items: 2}
+      }
+    });
 
-   /*----------------------------------------------------*/
-  	/* FitText Settings
-  	------------------------------------------------------ */
-  	setTimeout(function() {
+  }
 
-   	  $('main h1, #mod-about h1').fitText(1.1, { minFontSize: '28px', maxFontSize: '38px' });
+  
+if ($('#gmp').length) {
+  //===== Google Map =====//
+  var marker;
 
-  	}, 100);
+  function initMap() {
+    var map = new google.maps.Map(document.getElementById('gmp'), {
+      zoom: 13,
+      center: {lat: 59.325, lng: 18.070}
+    });
 
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      position: {lat: 59.327, lng: 18.067}
+    });
+    marker.addListener('click', toggleBounce);
+  }
 
-   /*---------------------------------------------------- */
-   /* ajaxchimp
-	------------------------------------------------------ */
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
 
-	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
-	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+  google.maps.event.addDomListener(window, 'load', initMap);
+}
 
-
-	$('#mc-form').ajaxChimp({
-
-		language: 'es',
-	   url: mailChimpURL
-
-	});
-
-	// Mailchimp translation
-	//
-	//  Defaults:
-	//	 'submit': 'Submitting...',
-	//  0: 'We have sent you a confirmation email',
-	//  1: 'Please enter a value',
-	//  2: 'An email address must contain a single @',
-	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
-	//  5: 'This email address looks fake or invalid. Please enter a real email address'
-
-	$.ajaxChimp.translations.es = {
-	  'submit': 'Submitting...',
-	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
-	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
-	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
-	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
-	}
-
-	/*---------------------------------------------------- */
-	/* Map
-	------------------------------------------------------ */
-	var latitude = 14.549072,
-		 longitude = 121.046958,
-		 map_zoom = 15,		 
-		 main_color = '#d8ac00',
-		 saturation_value= -30,
-		 brightness_value= 5,
-		 winWidth = $(window).width();		 
-
-   // marker url
-	if ( winWidth > 480 ) {
-		marker_url = 'images/icon-location-b.png';                    
-   } else {
-      marker_url = 'images/icon-location.png';            
-   }	 
-
-	// map style
-	var style = [ 
-		{
-			// set saturation for the labels on the map
-			elementType: "labels",
-			stylers: [
-				{ saturation: saturation_value }
-			]
-		},  
-	   {	// poi stands for point of interest - don't show these lables on the map 
-			featureType: "poi",
-			elementType: "labels",
-			stylers: [
-				{visibility: "off"}
-			]
-		},
-		{
-			// don't show highways lables on the map
-	      featureType: 'road.highway',
-	      elementType: 'labels',
-	      stylers: [
-	         { visibility: "off" }
-	      ]
-	   }, 
-		{ 	
-			// don't show local road lables on the map
-			featureType: "road.local", 
-			elementType: "labels.icon", 
-			stylers: [
-				{ visibility: "off" } 
-			] 
-		},
-		{ 
-			// don't show arterial road lables on the map
-			featureType: "road.arterial", 
-			elementType: "labels.icon", 
-			stylers: [
-				{ visibility: "off" }
-			] 
-		},
-		{
-			// don't show road lables on the map
-			featureType: "road",
-			elementType: "geometry.stroke",
-			stylers: [
-				{ visibility: "off" }
-			]
-		}, 
-		// style different elements on the map
-		{ 
-			featureType: "transit", 
-			elementType: "geometry.fill", 
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		}, 
-		{
-			featureType: "poi",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.government",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.sport_complex",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.attraction",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "poi.business",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "transit",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "transit.station",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "landscape",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-			
-		},
-		{
-			featureType: "road",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		},
-		{
-			featureType: "road.highway",
-			elementType: "geometry.fill",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		}, 
-		{
-			featureType: "water",
-			elementType: "geometry",
-			stylers: [
-				{ hue: main_color },
-				{ visibility: "on" }, 
-				{ lightness: brightness_value }, 
-				{ saturation: saturation_value }
-			]
-		}
-	];
-		
-	// map options
-	var map_options = {
-
-      	center: new google.maps.LatLng(latitude, longitude),
-      	zoom: 15,
-      	panControl: false,
-      	zoomControl: false,
-        	mapTypeControl: false,
-      	streetViewControl: false,
-      	mapTypeId: google.maps.MapTypeId.ROADMAP,
-      	scrollwheel: false,
-      	styles: style
-
-    	};
-
-   // inizialize the map
-	var map = new google.maps.Map(document.getElementById('map-container'), map_options);
-
-	// add a custom marker to the map				
-	var marker = new google.maps.Marker({
-
-		 	position: new google.maps.LatLng(latitude, longitude),
-		 	map: map,
-		 	visible: true,
-		 	icon: marker_url
-		 
-		});
-
-	// add custom buttons for the zoom-in/zoom-out on the map
-	function CustomZoomControl(controlDiv, map) {
-	
-		// grap the zoom elements from the DOM and insert them in the map 
-	 	var controlUIzoomIn= document.getElementById('map-zoom-in'),
-		  	 controlUIzoomOut= document.getElementById('map-zoom-out');
-
-		controlDiv.appendChild(controlUIzoomIn);
-		controlDiv.appendChild(controlUIzoomOut);
-
-		// Setup the click event listeners and zoom-in or out according to the clicked element
-		google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-			map.setZoom(map.getZoom()+1)
-		});
-		google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-			map.setZoom(map.getZoom()-1)
-		});
-			
-	}
-
-	var zoomControlDiv = document.createElement('div');
-	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
-
-	// insert the zoom div on the top right of the map
-	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(zoomControlDiv);	
-
-
-
-})(jQuery);
+});
